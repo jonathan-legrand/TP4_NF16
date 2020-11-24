ba270bb9ee61cf8bf7b687620037146926966862
jonathan-sudo <johnlegrand972@gmail.com>
2020-11-24 09:54:48 +0100

    création du menu principal

---
+++ src/main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define CHARGER_FICHIER 1
+#define CARACTERISTIQUES 2
+#define AFFICHER_INDEX 3
+#define RECHERCHER 4
+#define AFFICHER_OCCURENCES 5 
+#define EQUILIBRER 6
+#define QUITTER 7
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+
+{
+    int choix;
+    do {
+
+
+        /* affichage menu */
+        printf("1.Charger fichier\n"
+                "2.Caracteristiques\n"
+                "3.Afficher_index\n"
+                "4.Rechercher\n"
+                "5.Afficher_occurences\n"
+                "6.Equilibrer\n"
+                "7.Quitter\n"
+              );
+
+        fflush(stdin);
+        printf("\nVotre choix ? ");
+        scanf("%d",&choix);
+
+        /* suppression des caracteres dans stdin */
+
+        switch(choix)
+        {
+            case CHARGER_FICHIER:
+                printf("Choix 1\n");
+                break;
+
+            case CARACTERISTIQUES:
+                printf("Choix 2\n");
+                break;
+
+            case AFFICHER_INDEX:
+                printf("Choix 3\n");
+                break;
+
+            case RECHERCHER:
+                break;
+
+            case AFFICHER_OCCURENCES:
+                break;
+
+            case EQUILIBRER:
+                break;
+
+            case QUITTER:
+                break;
+
+            default:
+                printf("Choix erroné\n\n\n");
+        }
+    }while (choix != QUITTER); 
+
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+/* Conseil pour la manipulation des chaines de caractères. Les bibliothèques C contiennent plusieurs fonctions qui vous seront utiles lors du TP, vous avez <ctype.h> pour les fonctions sur les caractères et <string.h> pour les chaines de caractères. Avant d'implémenter vos propres traitements sur les chaines de caractères, vérifiez s'il existe des fonctions C qui le font. Ca vous éviteras du travail en plus. Si elles ne sont pas dans ces deux bibliothèques, elle peuvent être dans une autre. Google, stackoverflow et la documentation du langage C sont vos amis https://fr.cppreference.com/w/c Lien vers la documentation C 
+*/
