9b890663f1ab266aec74a8524c92f8bc6a318836
bapt380 <baptvlsup@gmail.com>
2020-11-24 10:00:47 +0100

    correction nom fichier + 2eme fonction

--- src/TP4.c
+++ src/TP4.c
@@ -1,4 +1,4 @@
-#include "../include/TP3.h"
+#include "../include/TP4.h"
 #include <stdio.h>
 
 /*De Youcef Amarouche à tout le monde:  09:25 AM
@@ -24,4 +24,33 @@ t_ListePositions* creer_liste_postions(){
 }
 
 
+int ajouter_position(t_ListePositions *listeP, int ligne, int ordre, int num_phrase){
+    t_ListePositions *positionEnCours = listeP;
+    
+    while (ligne>=positionEnCours->ligne)
+    {
+        positionEnCours=positionEnCours->suivant;
+    }
+    while (ordre>=positionEnCours->ordre)
+    {
+        positionEnCours=positionEnCours->suivant;
+    }
+    while (num_phrase>=positionEnCours)
+    {
+        positionEnCours=positionEnCours->suivant;
 
+    }
+
+    if ((ligne==positionEnCours->ligne) && (ordre==positionEnCours->ordre) && (num_phrase==positionEnCours))
+    {
+        printf("La position souhaitée exite déjà");
+        return 0;
+    }
+    else
+    {
+        printf("La position souhaitée a bien été ajoutée");
+        return 1;
+    }
+    
+    
+}
