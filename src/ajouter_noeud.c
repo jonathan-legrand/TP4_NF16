4329183fc0a0607c8de63a5a42d2247ffaf8d5b9
jonathan <johnlegrand972@gmail.com>
2020-12-08 17:46:57 -0400

    Un peu de ménage dans ajouter_noeud

--- src/ajouter_noeud.c
+++ src/ajouter_noeud.c
@@ -31,14 +31,9 @@ t_Noeud *rechercher_noeud(t_Index *index, t_Noeud *noeud)
     char *nouvChaine = malloc(sizeof(0));
     char *nouvChaine2 = malloc(sizeof(0));
     t_Noeud *noeudEnCours = index->racine;
-    //printf("\nla racine : %s",index->racine->mot);
-    /*if (index->racine->filsDroit!=NULL)
-        printf("\nle fils droit est %s\n",index->racine->filsDroit->mot);*/
     int cmpChar;
     while(noeudEnCours != NULL){
 
-        //printf("\nLe mot en cours est %s",noeudEnCours->mot);
-        //printf("\nLe mot à chercher est %s",noeud->mot);
 
         cmpChar = strcmp(strtolower(nouvChaine,noeudEnCours->mot),strtolower(nouvChaine2,noeud->mot));
         if (!cmpChar){
@@ -48,24 +43,16 @@ t_Noeud *rechercher_noeud(t_Index *index, t_Noeud *noeud)
         else if(cmpChar<0){ 
             // noeudEnCours->mot est lexicalement plus petit que noeud->mot
             if(noeudEnCours->filsDroit == NULL){
-                //printf("\nfils droit null");
                 return(noeudEnCours);
             }
-            //printf("\nfils droit non null\n");
-            //printf("Fils droit : %s\n",noeudEnCours->filsDroit->mot);
             noeudEnCours = noeudEnCours->filsDroit;
         } else if(cmpChar>0){
             // noeudEnCours->mot est lexicalement plus grand que noeud->mot
             if(noeudEnCours->filsGauche == NULL){
-                //printf("\nfils gauche null");
                 return(noeudEnCours);
             }
-            //printf("\nfils gauche non null");
             noeudEnCours = noeudEnCours->filsGauche;
         }
-        //printf("\n fin itér: Le mot en cours sera %s",noeudEnCours->mot); 
-        //printf("\n fin itér: Le mot à chercher est %s",noeud->mot);
-        //motEnCours=malloc(0);
     }
     return noeudEnCours;
 }
@@ -89,7 +76,6 @@ t_Noeud* ajouter_noeud ( t_Index *index, t_Noeud *noeud)
     {
         
         index->racine=creer_noeud();
-        // it's a deep copy
         index->racine->mot = strdup (noeud->mot);
         return index->racine;
     }
@@ -100,15 +86,14 @@ t_Noeud* ajouter_noeud ( t_Index *index, t_Noeud *noeud)
         noeudPrecedent = rechercher_noeud(index,noeud);
         
 
-        if (noeudPrecedent == NULL){ // si c'est null ça veut dire que le mot existe deja confirmation ???
+        if (noeudPrecedent == NULL){ 
             printf("Erreur lors de l'ajout du noeud\n");
-
             return NULL;
         }
 
         int cmpChar;
-        char *nouvChaine = malloc(sizeof(0));
-        char *nouvChaine2 = malloc(sizeof(0));
+        char *nouvChaine = malloc(sizeof(noeudPrecedent->mot));
+        char *nouvChaine2 = malloc(sizeof(noeud->mot));
         cmpChar = strcmp(strtolower(nouvChaine,noeudPrecedent->mot),strtolower(nouvChaine2,noeud->mot));
         *(noeud->mot+0)=toupper(*(noeud->mot+0)); // Première lettre en maj ( le 0 est inutile c'est juste pour la compréhension)
 
@@ -117,13 +102,11 @@ t_Noeud* ajouter_noeud ( t_Index *index, t_Noeud *noeud)
             noeudPrecedent->filsDroit=creer_noeud();
             noeudPrecedent->filsDroit->mot = strdup (noeud->mot);
             printf("\nLe noeud a bien été ajouté\n");
-            //printf("\nle fils droit qui vient d'être ajouté : %s\n",index->racine->filsDroit->mot);
             return noeudPrecedent->filsDroit;
         }
         else if(cmpChar>0) {  
             // noeudPrecedent->mot est lexicalement plus grand que mot
             printf("\nLe noeud a bien été ajouté\n");
-            //noeudPrecedent->filsGauche = noeud;
             noeudPrecedent->filsGauche=creer_noeud();
             noeudPrecedent->filsGauche->mot = strdup (noeud->mot);
             return noeudPrecedent->filsGauche;
